81119b65eb42f378ef6caf0d0dda23c37ff260e9
wangkaiwd <1162486176@qq.com>
2019-04-02 10:34:58 +0800

    feat: User Collections 添加role字段

--- models/User.js
+++ models/User.js
@@ -7,6 +7,7 @@ const UserSchema = new mongoose.Schema({
   password: { type: String, required: true },
   email: { type: String, required: true },
   avatar: { type: String },
+  role: { type: String, required: true },
   date: { type: Date, default: Date.now }
 });
 
