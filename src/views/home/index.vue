57935e4708423387c8840a463b7217f13258b651
wangkaiwd <1162486176@qq.com>
2019-04-06 20:51:08 +0800

    feat: 页面区域上中下布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,19 +1,24 @@
 <template>
-  <el-row class="home">
+  <el-row type="flex" class="admin-home">
     <left-nav></left-nav>
-    <el-col>
-      <router-view></router-view>
+    <el-col class="admin-home-content" :span="20">
+      <admin-header></admin-header>
+      <el-card class="admin-home-content-page">
+        <router-view></router-view>
+      </el-card>
+      <admin-footer></admin-footer>
     </el-col>
   </el-row>
 </template>
 
 <script>
   import AdminHeader from 'layouts/header'
+  import AdminFooter from 'layouts/footer'
   import LeftNav from 'layouts/leftNav'
 
   export default {
     name: 'AdminHome',
-    components: { AdminHeader, LeftNav },
+    components: { AdminHeader, AdminFooter, LeftNav },
     data () {
       return {}
     },
@@ -24,7 +29,13 @@
 </script>
 
 <style lang="scss" scoped>
-  .home {
-
+  .admin-home {
+    &-content {
+      display: flex;
+      flex-direction: column;
+    }
+    &-content-page {
+      flex: 1;
+    }
   }
 </style>
