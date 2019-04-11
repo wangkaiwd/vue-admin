d652be73f226d1815e63839b6d55255bc9374422
wangkaiwd <1162486176@qq.com>
2019-04-11 15:07:05 +0800

    feat: 提取面包屑组件

---
+++ src/layouts/breadcrumb/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <el-breadcrumb separator="/" class="admin-home-content-breadcrumb">
+    <!--  $route.matched: 一个数组，包含当前路由的所有嵌套路径片段的路由记录，即当前路由以及当前路由对应的所有父级路由对象  -->
+    <el-breadcrumb-item
+      v-for="route in $route.matched"
+      :to="{path:route.path}"
+      :key="route.path"
+      v-if="route.name"
+    >
+      {{route.name}}
+    </el-breadcrumb-item>
+  </el-breadcrumb>
+</template>
+
+<script>
+  export default {
+    name: 'AdminBread'
+  };
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -7,17 +7,7 @@
         <div class="admin-home-content-page-header">
           <!-- this里的数据都是响应式的，当this.$route发生改变的时候，对应的页面就会重新render,更新状态   -->
           <strong>{{$route.name}}</strong>
-          <el-breadcrumb separator="/" class="admin-home-content-breadcrumb">
-            <!--  $route.matched: 一个数组，包含当前路由的所有嵌套路径片段的路由记录，即当前路由以及当前路由对应的所有父级路由对象  -->
-            <el-breadcrumb-item
-              v-for="route in $route.matched"
-              :to="{path:route.path}"
-              :key="route.path"
-              v-if="route.name"
-            >
-              {{route.name}}
-            </el-breadcrumb-item>
-          </el-breadcrumb>
+          <admin-bread></admin-bread>
         </div>
         <router-view></router-view>
       </el-card>
@@ -29,13 +19,14 @@
 <script>
   import AdminHeader from 'layouts/header';
   import AdminFooter from 'layouts/footer';
+  import AdminBread from 'layouts/breadcrumb';
   import LeftNav from 'layouts/leftNav';
   import { mapMutations } from 'vuex';
   import { goLogin } from 'utils/user';
 
   export default {
     name: 'AdminHome',
-    components: { AdminHeader, AdminFooter, LeftNav },
+    components: { AdminHeader, AdminFooter, LeftNav, AdminBread },
     data () {
       return {};
     },
@@ -64,8 +55,6 @@
       display: flex;
       flex-direction: column;
       min-width: 600px;
-    }
-    &-content-breadcrumb {
     }
     &-content-page {
       flex: 1;
