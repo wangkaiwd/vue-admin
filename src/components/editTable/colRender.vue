dc796c13d328f649d91df358bfb2142719eec6de
wangkaiwd <1162486176@qq.com>
2019-04-18 18:09:18 +0800

    feat: 1. 加入本地mock  2. 添加可编辑表格组件

---
+++ src/components/editTable/colRender.vue
@@ -0,0 +1,23 @@
+<script>
+  export default {
+    name: 'ColRender',
+    functional: true, // 函数式组件
+    props: {
+      row: Object,
+      render: Function,
+      index: Number,
+      column: {
+        type: Object,
+        default: () => ({})
+      }
+    },
+    render (h, ctx) {
+      const { row, index, column, render } = ctx.props;
+      return render(h, { row, index, column });
+    }
+  };
+</script>
+
+<style scoped>
+
+</style>
