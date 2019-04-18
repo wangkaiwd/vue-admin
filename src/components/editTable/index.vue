9a4e346e58ed8d2a3cbe7d83dcb60eb3b7ed8aa2
wangkaiwd <1162486176@qq.com>
2019-04-18 11:22:38 +0800

    feat: 表格编辑需求分析

---
+++ src/components/editTable/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="edit-table">
+    可编辑表格
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'EditTable'
+  };
+</script>
+
+<style lang="scss" scoped>
+  .edit-table {
+    
+  }
+</style>
